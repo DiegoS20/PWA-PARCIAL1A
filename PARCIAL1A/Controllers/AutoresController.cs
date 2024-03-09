649987c6d8931cd03035b9bde68699eab3d25785
Rickmonte2022 <ricardo.monterrosa@catolica.edu.sv>
2024-03-09 13:44:59 -0600

    Controladores de autores y autores libro

---
+++ PARCIAL1A/Controllers/AutoresController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PARCIAL1A.Models;
+
+namespace PARCIAL1A.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+
+
+        private readonly Parcial1aContext _parcial1Adb;
+
+        public AutoresController(Parcial1aContext parcial1Adb)
+        {
+            _parcial1Adb = parcial1Adb;
+        }
+
+        //lectura
+        [HttpGet]
+        [Route("GetAll")]
+
+        public IActionResult Get()
+        {
+            List<Autor> listadoAutor = (from e in _parcial1Adb.Autores select e).ToList();
+
+            if (listadoAutor.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoAutor);
+        }
+
+        //Agregar
+        [HttpPost]
+        [Route("Add")]
+
+
+        public IActionResult GuardarAutorL([FromBody] Autor autor)
+        {
+            try
+            {
+                _parcial1Adb.Autores.Add(autor);
+                _parcial1Adb.SaveChanges();
+                return Ok(autor);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        //modificar
+        [HttpPut]
+        [Route("Actualizar/{id}")]
+
+        public IActionResult actualizarAutor(int id, [FromBody] Autor modificarAutor)
+        {
+            AutorLibro? AutorActual = (from e in _parcial1Adb.AutorLibros where e.AutorId == id select e).FirstOrDefault();
+
+            if (AutorActual == null) { return NotFound(); }
+
+            AutorActual.Orden = AutorActual.Orden;
+            AutorActual.Orden = AutorActual.Orden;
+
+
+
+            _parcial1Adb.Entry(AutorActual).State = EntityState.Modified;
+            _parcial1Adb.SaveChanges();
+
+            return Ok(modificarAutor);
+
+        }
+
+        //borrar
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public IActionResult EliminarAutor(int id)
+        {
+
+            Autor? eliminarAutor = (from e in _parcial1Adb.Autores where e.Id  == id select e).FirstOrDefault();
+
+            if (eliminarAutor == null)
+                return NotFound();
+
+            _parcial1Adb.Autores.Attach(eliminarAutor);
+            _parcial1Adb.Autores.Remove(eliminarAutor);
+            _parcial1Adb.SaveChanges();
+
+            return Ok(eliminarAutor);
+        }
+    }
+}
---
+++ PARCIAL1A/Controllers/AutoresLibroController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PARCIAL1A.Models;
+
+namespace PARCIAL1A.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresLibroController : ControllerBase
+    {
+
+        private readonly Parcial1aContext _parcial1Adb;
+
+        public AutoresLibroController(Parcial1aContext parcial1Adb)
+        {
+            _parcial1Adb = parcial1Adb;
+        }
+
+        //lectura
+        [HttpGet]
+        [Route("GetAll")]
+
+        public IActionResult Get()
+        {
+            List<AutorLibro> listadoAutorL = (from e in _parcial1Adb.AutorLibros select e).ToList();
+
+            if (listadoAutorL.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoAutorL);
+        }
+
+        //Agregar
+        [HttpPost]
+        [Route("Add")]
+
+
+        public IActionResult GuardarAutorL([FromBody] AutorLibro autorLibro)
+        {
+            try
+            {
+                _parcial1Adb.AutorLibros.Add(autorLibro);
+                _parcial1Adb.SaveChanges();
+                return Ok(autorLibro);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        //modificar
+        [HttpPut]
+        [Route("Actualizar/{id}")]
+
+        public IActionResult actualizarAutorL(int id, [FromBody] AutorLibro modificarAutorL)
+        {
+            AutorLibro? AutorlActual = (from e in _parcial1Adb.AutorLibros where e.AutorId == id select e).FirstOrDefault();
+
+            if (AutorlActual == null) { return NotFound(); }
+
+            AutorlActual.AutorId = AutorlActual.AutorId;
+            AutorlActual.LibroId = AutorlActual.LibroId;
+
+
+
+            _parcial1Adb.Entry(AutorlActual).State = EntityState.Modified;
+            _parcial1Adb.SaveChanges();
+
+            return Ok(modificarAutorL);
+
+        }
+
+        //borrar
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+        public IActionResult EliminarAutor(int id)
+        {
+
+            AutorLibro? eliminarAutor = (from e in _parcial1Adb.AutorLibros where e.AutorId == id select e).FirstOrDefault();
+
+            if (eliminarAutor == null)
+                return NotFound();
+
+            _parcial1Adb.AutorLibros.Attach(eliminarAutor);
+            _parcial1Adb.AutorLibros.Remove(eliminarAutor);
+            _parcial1Adb.SaveChanges();
+
+            return Ok(eliminarAutor);
+        }
+
+    }
+}
